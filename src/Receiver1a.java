dd7abeeb41e0a6c29e1e8bc6d67a0e52fa8d6497
battery233 <31448923+Battery233@users.noreply.github.com>
2019-02-10 21:43:07 +0000

    Add code for part 1a: Basic framework (large file transmission under ideal conditions)

---
+++ src/Receiver1a.java
@@ -0,0 +1,48 @@
+/* Chenghao Ye s1786987 */
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Receiver1a {
+
+    // define size here
+    private static final int DATA_SIZE = 1024;
+    private static final int PACkET_SIZE = DATA_SIZE + 5;   // head size = 5
+
+    public static void main(String[] args) {
+        System.out.println("Port: " + args[0] + " Filename: " + args[1]);
+        receiver(Integer.parseInt(args[0]), args[1]);
+    }
+
+    private static void receiver(int Port, String fileName) {
+        try {
+            // get tools ready
+            DatagramSocket socket = new DatagramSocket(Port);
+            FileOutputStream writeFile = new FileOutputStream(new File(fileName));
+
+            boolean eof = false;    // flag for the last packet
+            while (!eof) {
+                byte[] data = new byte[PACkET_SIZE];
+                DatagramPacket received = new DatagramPacket(data, PACkET_SIZE);
+                socket.receive(received);
+                data = received.getData();
+                int length = received.getLength();  // real length of the packet size
+                int sequence = (data[2] & 0xff) << 8 | (data[3] & 0xff);    // get sequence num from bytes
+                eof = 1 == (data[4] & 0xff);    // get flag
+                if (!eof) {
+                    writeFile.write(data, PACkET_SIZE - DATA_SIZE, DATA_SIZE);
+                    System.out.println("Packet No." + sequence + " got! Length = " + length);
+                } else {
+                    System.out.println("Last sequence number is: " + sequence + " length = " + length);
+                    writeFile.write(data, PACkET_SIZE - DATA_SIZE, length - (PACkET_SIZE - DATA_SIZE));
+                    socket.close();
+                    writeFile.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Sender1a.java
@@ -0,0 +1,79 @@
+/* Chenghao Ye s1786987 */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.*;
+
+import static java.lang.Thread.sleep;
+
+public class Sender1a {
+
+    // define size here
+    private static final int DATA_SIZE = 1024;
+    private static final int PACkET_SIZE = DATA_SIZE + 5;   // head size = 5
+    private static final int SLEEP_TIME = 10;
+
+    public static void main(String[] args) {
+        System.out.println("RemoteHost: " + args[0] + " Port: " + args[1] + " Filename: " + args[2]);
+        try {
+            transport(InetAddress.getByName(args[0]), Integer.valueOf(args[1]), args[2]);
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown host: " + args[0]);
+        }
+    }
+
+    private static void transport(InetAddress RemoteHost, int Port, String Filename) {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            File file = new File(Filename);
+
+            // calculate the number of packets needed
+            long number = file.length() / DATA_SIZE;
+            if (file.length() % DATA_SIZE != 0) {
+                number++;
+            }
+            System.out.println("File size: " + file.length() + " packets number: " + number);
+
+            // read file
+            byte[] fileBytes = new byte[(int) file.length()];
+            FileInputStream fis = new FileInputStream(file);
+            fis.read(fileBytes);
+            fis.close();
+
+            int i, j;
+            for (i = 0; i < number; i++) {
+                byte[] packet;
+                if (i < number - 1) {
+                    packet = new byte[PACkET_SIZE];
+                    // eof flag
+                    packet[4] = 0;
+                    for (j = 0; j < DATA_SIZE; j++) {
+                        packet[j + 5] = fileBytes[DATA_SIZE * i + j];
+                    }
+                } else {
+                    packet = new byte[(int) (file.length() % DATA_SIZE) + 5];
+                    //eof flag
+                    packet[4] = 1;
+                    for (j = 0; j < file.length() - DATA_SIZE * i; j++) {
+                        packet[j + 5] = fileBytes[DATA_SIZE * i + j];
+                    }
+                }
+                // two header value
+                packet[0] = 0;
+                packet[1] = 0;
+                //sequence number
+                packet[2] = (byte) (i >> 8);
+                packet[3] = (byte) i;
+                socket.send(new DatagramPacket(packet, packet.length, RemoteHost, Port));
+                System.out.println("Packet No." + i + " sent! size = " + packet.length);
+                // sleep time
+                sleep(SLEEP_TIME);
+            }
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
