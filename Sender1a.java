0d94965d1bdc5ec220a8183c103be24c19a6b3b1
battery233 <31448923+Battery233@users.noreply.github.com>
2019-02-10 15:16:13 +0000

    Add skeleton files

---
+++ Sender1a.java
@@ -0,0 +1,10 @@
+/* Chenghao Ye s1786987 */
+
+public class Sender1a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
